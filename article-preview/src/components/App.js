5afb6bbb38b37837706fa299e202c978556955ae
Geraldine <hello@geraldiner.com>
2021-06-18 18:55:29 -1000

    initial commit

---
+++ article-preview/src/components/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import avatar from "../assets/images/avatar-michelle.jpg";
+import cover from "../assets/images/drawers.jpg";
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <div className="preview">
+        <div className="cover-img">
+          <img src={cover} alt="Desk with vase decor" />
+        </div>
+        <div className="description">
+          <h3>
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home
+          </h3>
+          <p>
+            Ever been in a room and felt like something was missing? Perhaps it
+            felt slightly bare and uninviting. I’ve got some simple tips to help
+            you make any room feel complete.
+          </p>
+          <div className="post-meta">
+            <div className="author-info">
+              <img src={avatar} alt="Author avatar" className="avatar" />
+              <p>
+                <span className="author-name">Michelle Appleton</span>
+                <span className="post-date">28 Jun 2020</span>
+              </p>
+            </div>
+            <div className="social-share">
+              <span className="share-btn">Share</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/geraldiner">Geraldine</a>.
+      </div>
+    </div>
+  );
+};
+
+export default App;
